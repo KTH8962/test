bafe19ca077a000fcabedfb475eb7cfa33fb6d99
Kims430 <aakimaa28@naver.com>
2024-09-10 12:23:23 +0900

    2024.09.10 담담

---
+++ test1/bin/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Member2(name, password, email) VALUES('James', '1234', 'james@email.com');
+INSERT INTO Member2(name, password, email) VALUES('Hans', '1234', 'hans@email.com');
+INSERT INTO Member2(name, password, email) VALUES('Sumi', '1234', 'sumi@email.com');
